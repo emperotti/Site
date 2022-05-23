e3ae76c03fdec36a29462625daae39d690f391f1
Eduardo Miguel Perotti Oliveira <emperotti@hotmail.com>
2022-05-23 20:29:51 -0300

    Versao_2

--- js.js
+++
@@ -1,7 +0,0 @@
-const image = document.querySelector('wpp');
-image.addEventListener('click', (event) => {
-  const { src } = event.target;
-  alert(`Atributo de origem da imagem: "${src}".`);
-  // Redirecionar para outra página:
-  location.replace(src);
-});
